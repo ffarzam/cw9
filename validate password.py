85849887eb971c2de552a3a15a431b7527f506e4
Farzam <ffarzam_1992@yahoo.com>
2023-06-02 11:35:59 +0330

    password validation

---
+++ validate password.py
@@ -0,0 +1,9 @@
+import re
+
+
+def validate_password(pwd):
+    password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%_^&.*-]).{8,}$"
+    match = re.match(password_pattern, pwd)
+    return bool(match)
+
+print(validate_password("fFarzam_1992"))
